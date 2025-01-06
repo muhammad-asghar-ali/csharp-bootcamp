9eb584446c0fc9269b70514342134d37e6d89460
muhammad-asghar-ali <masgharali.eng@gmail.com>
2025-01-06 15:26:16 +0500

    chore(): classes and structs

--- basics/Program.cs
+++ basics/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Globalization;
 using System.Text;
 
-namespace Basics
+namespace basics
 {
     public class Program
     {
@@ -403,6 +403,37 @@ namespace Basics
             // -------------------------------------------- ENUM --------------------------------------------
             CarColor blueCar = CarColor.Blue;
             PaintCar(blueCar);
+
+            // -------------------------------------------- Structs --------------------------------------------
+            // Create a Rectangle
+            Rectangle rect1;
+
+            // Add values to it and run the Area method
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1 : {0}", rect1.Area());
+
+            // Use a constructor to create a Rectangle
+            Rectangle rect2 = new(100, 40);
+            Console.WriteLine("rect2.length : {0}", rect2.length);
+
+            // -------------------------------------------- OBJECT ORIENTED PROGRAMMING --------------------------------------------
+            // A class models real world objects by defining their attributes (fields) and capabilities (methods) 
+            // Then unlike with structs you can  inherit from a class and create more specific subclass types
+            // Add a class Project -> Add Class
+            // Create an Animal object You could also assign values like fox.name = "Red"
+            Animal fox = new()
+            {
+                name = "Red",
+                sound = "Raaaw"
+            };
+
+            // Call the static method
+            Console.WriteLine("# of Animals {0}", Animal.GetNumAnimals());
+
+            // You can also create static utility classes Project -> Add Class
+            Console.WriteLine("Area of Rectangle : {0}", ShapeMath.GetArea("rectangle", 5, 6));
+
         }
 
         // ----- FUNCTIONS -----
@@ -495,6 +526,26 @@ namespace Basics
             Green,
             Red,
             Yellow
+        }
+
+        // ----- STRUCTS -----
+        // A struct is a user defined type that contain multiple fields and methods
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            // You can create a constructor method that will set the values for fields
+            public Rectangle(double l = 1, double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
         }
 
     }
