a2f95fee679492a389e5a895610644cbb6688997
muhammad-asghar-ali <masgharali.eng@gmail.com>
2025-01-08 16:51:38 +0500

    chore(): entity core crud

--- EMS/Program.cs
+++ EMS/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EMS.Models;
+using EMS.Data;
 
 namespace EMS
 {
@@ -9,7 +11,48 @@ namespace EMS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            using var db = new AppDbContext();
+
+            // insert data
+            Manager manager = new();
+            manager.FirstName = "John";
+            manager.LastName = "Doe";
+
+            db.Managers.Add(manager);
+            db.SaveChanges();
+
+            Employee emp = new();
+            emp.FirstName = "Alex";
+            emp.LastName = "John";
+            emp.Salary = 1000;
+            emp.ManagerId = 1;
+
+            db.Employees.Add(emp);
+            db.SaveChanges();
+
+            // retrive data
+            var employees = db.Employees.ToList();
+            foreach (var ep in employees)
+            {
+                Console.WriteLine(ep.FirstName);
+                Console.WriteLine(ep.Salary);
+            }
+
+            var employee = db.Employees.Single(e => e.Id == 1);
+            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+            // update data
+            var update = db.Employees.Single(e => e.Id == 1);
+            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+            update.Salary = 80000;
+            db.SaveChanges();
+            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+            // delete data
+            var mng = db.Managers.Single(m => m.Id == manager.Id);
+            db.Managers.Remove(mng);
+            db.SaveChanges();
         }
     }
 }
