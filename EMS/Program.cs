7a433c1b7b01f944f7d9e8ea68115e0ec1fd5979
muhammad-asghar-ali <masgharali.eng@gmail.com>
2025-01-08 17:15:27 +0500

    chore(): eager loading

--- EMS/Program.cs
+++ EMS/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using EMS.Models;
 using EMS.Data;
 
@@ -13,46 +14,64 @@ namespace EMS
         {
             using var db = new AppDbContext();
 
-            // insert data
-            Manager manager = new();
-            manager.FirstName = "John";
-            manager.LastName = "Doe";
+            // // insert data
+            // Manager manager = new();
+            // manager.FirstName = "John";
+            // manager.LastName = "Doe";
 
-            db.Managers.Add(manager);
-            db.SaveChanges();
+            // db.Managers.Add(manager);
+            // db.SaveChanges();
 
-            Employee emp = new();
-            emp.FirstName = "Alex";
-            emp.LastName = "John";
-            emp.Salary = 1000;
-            emp.ManagerId = 1;
+            // Employee emp = new();
+            // emp.FirstName = "Alex";
+            // emp.LastName = "John";
+            // emp.Salary = 1000;
+            // emp.ManagerId = 1;
 
-            db.Employees.Add(emp);
-            db.SaveChanges();
+            // db.Employees.Add(emp);
+            // db.SaveChanges();
 
-            // retrive data
-            var employees = db.Employees.ToList();
-            foreach (var ep in employees)
+            // // retrive data
+            // var employees = db.Employees.ToList();
+            // foreach (var ep in employees)
+            // {
+            //     Console.WriteLine(ep.FirstName);
+            //     Console.WriteLine(ep.Salary);
+            // }
+
+            // var employee = db.Employees.Single(e => e.Id == 1);
+            // Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+            // // update data
+            // var update = db.Employees.Single(e => e.Id == 1);
+            // Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+            // update.Salary = 80000;
+            // db.SaveChanges();
+            // Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+
+            // // delete data
+            // var mng = db.Managers.Single(m => m.Id == manager.Id);
+            // db.Managers.Remove(mng);
+            // db.SaveChanges();
+
+            //------------------------------------------- Loading -------------------------------------------
+            // eager loading
+            var emps = db.Employees.Include(e => e.EmployeeDetails).ToList();
+            foreach (var epx in emps)
             {
-                Console.WriteLine(ep.FirstName);
-                Console.WriteLine(ep.Salary);
+                Console.WriteLine(epx.EmployeeDetails.Email);
             }
 
-            var employee = db.Employees.Single(e => e.Id == 1);
-            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
-
-            // update data
-            var update = db.Employees.Single(e => e.Id == 1);
-            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
-
-            update.Salary = 80000;
-            db.SaveChanges();
-            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
-
-            // delete data
-            var mng = db.Managers.Single(m => m.Id == manager.Id);
-            db.Managers.Remove(mng);
-            db.SaveChanges();
+            var projects = db.Projects.Include(e => e.EmployeeProjects).ThenInclude(e => e.Employee).ToList();
+            foreach (var project in projects)
+            {
+                Console.WriteLine($"Project Name: {project.Name}");
+                foreach (var empproj in project.EmployeeProjects)
+                {
+                    Console.WriteLine($"Employee Name: {empproj.Employee.FirstName}");
+                }
+            }
         }
     }
 }
