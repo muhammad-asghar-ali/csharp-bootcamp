2641cf75ba126fba6667b221f58ba88d44c8266b
muhammad-asghar-ali <masgharali.eng@gmail.com>
2025-01-08 18:19:28 +0500

    chore(): run full app

--- EMS/Program.cs
+++ EMS/Program.cs
@@ -14,53 +14,56 @@ namespace EMS
         {
             using var db = new AppDbContext();
 
-            // // insert data
-            // Manager manager = new();
-            // manager.FirstName = "John";
-            // manager.LastName = "Doe";
+            // insert data
+            Manager manager = new();
+            manager.FirstName = "John";
+            manager.LastName = "Doe";
 
-            // db.Managers.Add(manager);
-            // db.SaveChanges();
+            db.Managers.Add(manager);
+            db.SaveChanges();
 
-            // Employee emp = new();
-            // emp.FirstName = "Alex";
-            // emp.LastName = "John";
-            // emp.Salary = 1000;
-            // emp.ManagerId = 1;
+            Employee emp = new();
+            emp.FirstName = "Alex";
+            emp.LastName = "John";
+            emp.Salary = 1000;
+            emp.ManagerId = 1;
 
-            // db.Employees.Add(emp);
-            // db.SaveChanges();
+            db.Employees.Add(emp);
+            db.SaveChanges();
 
-            // // retrive data
-            // var employees = db.Employees.ToList();
-            // foreach (var ep in employees)
-            // {
-            //     Console.WriteLine(ep.FirstName);
-            //     Console.WriteLine(ep.Salary);
-            // }
+            // retrive data
+            var employees = db.Employees.ToList();
+            foreach (var ep in employees)
+            {
+                Console.WriteLine(ep.FirstName);
+                Console.WriteLine(ep.Salary);
+            }
 
-            // var employee = db.Employees.Single(e => e.Id == 1);
-            // Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+            var employee = db.Employees.Single(e => e.Id == 1);
+            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
 
-            // // update data
-            // var update = db.Employees.Single(e => e.Id == 1);
-            // Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+            // update data
+            var update = db.Employees.Single(e => e.Id == 1);
+            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
 
-            // update.Salary = 80000;
-            // db.SaveChanges();
-            // Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
+            update.Salary = 80000;
+            db.SaveChanges();
+            Console.WriteLine("{0}, {1}, {2}", employee.FirstName, employee.LastName, employee.Salary);
 
-            // // delete data
-            // var mng = db.Managers.Single(m => m.Id == manager.Id);
-            // db.Managers.Remove(mng);
-            // db.SaveChanges();
+            // delete data
+            var mng = db.Managers.Single(m => m.Id == manager.Id);
+            db.Managers.Remove(mng);
+            db.SaveChanges();
 
             //------------------------------------------- Loading -------------------------------------------
             // eager loading
             var emps = db.Employees.Include(e => e.EmployeeDetails).ToList();
             foreach (var epx in emps)
             {
-                Console.WriteLine($"Email: {epx.EmployeeDetails.Email}");
+                if (epx.EmployeeDetails != null)
+                {
+                    Console.WriteLine($"Email: {epx.EmployeeDetails.Email}");
+                }
             }
 
             var projects = db.Projects.Include(e => e.EmployeeProjects).ThenInclude(e => e.Employee).ToList();
@@ -79,7 +82,10 @@ namespace EMS
             foreach (var expt in empsExpts)
             {
                 db.Entry(expt).Reference(e => e.EmployeeDetails).Load(); // for one-to-one use Reference
-                Console.WriteLine($"ID: {expt.Id} Email: {expt.EmployeeDetails.Email}");
+                if (expt.EmployeeDetails != null)
+                {
+                    Console.WriteLine($"ID: {expt.Id} Email: {expt.EmployeeDetails.Email}");
+                }
             }
 
             var managerx = db.Managers.ToList();
