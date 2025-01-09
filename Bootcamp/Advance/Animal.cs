52ce73f7becddaf777364f350222957639386281
muhammad-asghar-ali <masgharali.eng@gmail.com>
2025-01-09 15:29:31 +0500

    chore(): IEnumerable, Operator overloading and Anonymous types

---
+++ Bootcamp/Advance/Animal.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advance
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name = "No Name")
+        {
+            Name = name;
+        }
+    }
+}
